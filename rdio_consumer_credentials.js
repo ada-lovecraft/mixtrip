f74e58776c0aff2b70b41f9e6ef4491713260af6
Jeremy Dowell <jeremy@codevinsky.com>
2013-05-03 12:18:44 -0500

    removed private info

--- rdio_consumer_credentials.js
+++
@@ -1,6 +0,0 @@
-// You can get these by signing up for a developer account at:
-// http://developer.rdio.com/
-
-/* DON'T BE A DICK. DON'T USE MY KEYS */
-exports.RDIO_CONSUMER_KEY = "8cqh2xzc5m32u8awqahbkt2p"
-exports.RDIO_CONSUMER_SECRET = "7nXS37CH2Y"
